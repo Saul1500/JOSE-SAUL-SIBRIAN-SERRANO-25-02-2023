24a9eb25b9b778901820f4cf1d8c31e868202384
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-02-23 22:00:47 -0600

    Add files via upload

---
+++ AsincronaS5/BucleFor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            
+            int resultado;
+
+            Console.WriteLine("Ingrese el Primer Numero: ");
+            Console.WriteLine("Ingrese el Segundo Numero: ");
+            
+            for (int i = 1; i <= 0; i++)
+            {
+                resultado = i % 2;
+                Console.WriteLine(resultado.ToString());
+            }
+        }
+    }
+}
